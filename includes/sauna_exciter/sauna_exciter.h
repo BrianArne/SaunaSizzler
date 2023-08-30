7b467827e19247682a097438bdcd89cc8d09cc3e
Esteban Gomez <esteban.gomez@voicemod.net>
2023-08-30 22:38:18 +0300

    Add dummy Exciter implementation

--- includes/sauna_exciter/sauna_exciter.h
+++ includes/sauna_exciter/sauna_exciter.h
@@ -13,4 +13,43 @@
 */
 
 #pragma once
-#include "Exciter.h"
+#include <algorithm>
+
+namespace sauna {
+
+class Exciter {
+public:
+    Exciter();
+    ~Exciter();
+    
+    // No copy semantics
+    Exciter(const Exciter&) = delete;
+    const Exciter& operator=(const Exciter&) = delete;
+    
+    // No move semantics
+    Exciter(Exciter&&) = delete;
+    const Exciter& operator=(Exciter&&) = delete;
+    
+    void prepare(double newSampleRate) {
+        sampleRate = newSampleRate;
+    }
+
+    void process(float* const* output, const float* const* input, unsigned int numChannels, unsigned int numSamples) {
+        
+        // To avoid using it in more than 2 channels
+        numChannels = std::min(numChannels, 2u);
+        
+        for (unsigned int channel = 0; channel < numChannels; channel++)
+        {
+            for (unsigned int sample = 0; sample < numSamples; sample++)
+            {
+                // Unity gain for testing
+                output[channel][sample] = 1.0 * input[channel][number];
+            }
+        }
+    }
+    
+private:
+    double sampleRate {1.0};
+};
+} // end sauna namespace
