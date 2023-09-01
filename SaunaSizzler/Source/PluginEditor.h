6f00d44adb0fc2e7aa5a50666a0a5fc1372d4225
Sercan Atlı <hsercanatli@gmail.com>
2023-09-01 09:18:37 +0300

    Sercan/UI changes (#4)
    * Add button to switch between advance and simple mode
    
    * hmm, lets remove of
    
    * Change magic to big knob
    
    * Add labels on knobs
    
    ---------
    
    Co-authored-by: Sercan Atli <sercan.atli@avid.com>

--- SaunaSizzler/Source/PluginEditor.h
+++ SaunaSizzler/Source/PluginEditor.h
@@ -17,6 +17,7 @@
 /**
 */
 class SaunaSizzlerAudioProcessorEditor  : public juce::AudioProcessorEditor
+                                        , public juce::Button::Listener
 {
 public:
     SaunaSizzlerAudioProcessorEditor (SaunaSizzlerAudioProcessor&);
@@ -25,8 +26,12 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    
+    void buttonClicked (juce::Button* button) override;
 
 private:
+    void updateUIMode();
+    
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SaunaSizzlerAudioProcessor& audioProcessor;
@@ -34,13 +39,17 @@ private:
     juce::Component header;
     BigBoyDial bigBoyDial;
     
+    juce::TextButton magicButton;
+    
     juce::Component footer;
-    StandardDial lfoDial;
-    StandardDial reverbDial;
-    StandardDial steamDial;
+    StandardDial lfoDial { "LFO" };
+    StandardDial reverbDial { "Reverb" };
+    StandardDial steamDial { "Steam" };
     
     StandardDial smallLeftDial;
     StandardDial smallRightDial;
+    
+    bool advancedModeEnabled { false };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SaunaSizzlerAudioProcessorEditor)
 };
